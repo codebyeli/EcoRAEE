dadffd1c60d99175e1347b0ceefaa4d73661f274
codebyeli <e.encarnacion@nippy.la>
2024-12-06 23:13:22 -0400

    fix labels on landing page and about-us

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,17 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
   title = 'EcoRAEE';
   subtitle = 'Reciclaje Responsable de Residuos Electrónicos';
   description = `En EcoRAEE nos dedicamos al reciclaje y manejo adecuado de residuos de aparatos electrónicos. 
   Ayudamos a reducir la contaminación ambiental y a promover la reutilización de materiales valiosos.`;
-  mission = `Nuestra misión es transformar el impacto ambiental de los residuos electrónicos y contribuir a un futuro más sostenible.`;
+  vision = `Aspiramos a ser la plataforma líder en la gestión digital de residuos de aparatos eléctricos y electrónicos (RAEE), impulsando una transformación global hacia una economía circular y sostenible. Nos comprometemos a reducir los efectos negativos en el medio ambiente y a fomentar la responsabilidad ambiental en cada comunidad en la que operamos.`;
   services = [
     'Recolección de dispositivos electrónicos obsoletos.',
-    'Reciclaje y procesamiento de materiales peligrosos.',
-    'Promoción de la conciencia ambiental a través de programas educativos.'
+    'Promoción de la conciencia ambiental a través de programas educativos.',
   ];
 }
